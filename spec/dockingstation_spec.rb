0b33d5dffe7b1268deba62c168204d4b9321b1d8
George <george_whiting@hotmail.com>
2017-11-21 16:43:47 +0000

    ex 10 adds partial solution for testing if a bike has been created

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,5 +1,11 @@
 require "DockingStation.rb"
+require "Bike.rb"
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
+  it "Creates a new working bike object" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
