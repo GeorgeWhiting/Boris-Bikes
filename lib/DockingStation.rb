6a29ea02cc2db52ff5fad621dba886cfe3d3431e
George <george_whiting@hotmail.com>
2017-11-22 14:42:42 +0000

    adds test for occupied docking stations

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -3,4 +3,22 @@ class DockingStation
   def release_bike
     Bike.new
   end
+
+def initialize
+  @station_free = true
+end
+
+  def dock_bike
+
+    if @station_free == true
+      @station_free = false
+      return "Bike docked"
+    elsif @station_free == false
+      return "A bike is already there"
+    else
+      return "Neither"
+    end
+  end
+
+
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -7,4 +7,14 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it "Docks a bike" do
+    expect(subject.dock_bike).to eq "Bike docked"
+  end
+
+  it "Station is already full" do
+    subject.dock_bike
+    expect(subject.dock_bike).to eq "A bike is already there"
+  end
+
 end
