f5e41a7894d6be94330a8e857c16e9a531245147
George <george_whiting@hotmail.com>
2017-11-22 16:26:59 +0000

    adds exception when trying to pull a bike out of an empty station

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -4,25 +4,21 @@ class DockingStation
 attr_reader :bike
 
   def release_bike
-    Bike.new
+    raise "There are no bikes here" unless @bike
+    @bike
   end
 
-def initialize
-  @station_free = true
-end
+  def initialize
+    @station_free = true
+  end
 
   def dock_bike(bike)
-
     if @station_free == true
       @station_free = false
       @bike = bike
       return "Bike docked"
-    elsif @station_free == false
+    else @station_free == false
       return "A bike is already there"
-    else
-      return "Neither"
     end
   end
-
-
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -3,20 +3,36 @@ require "DockingStation.rb"
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  it "Creates a new working bike object" do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect(subject.release_bike).to eq bike
+    end
   end
 
+
+
+    it "Creates a new working bike object" do
+      bike = Bike.new
+      expect(bike).to be_working
+    end
+
   it "Docks a bike" do
-    bike = subject.release_bike
+    bike = Bike.new
     expect(subject.dock_bike(bike)).to eq "Bike docked"
   end
 
   it "Station is already full" do
-    bike = subject.release_bike
+    bike = Bike.new
     subject.dock_bike(bike)
     expect(subject.dock_bike(bike)).to eq "A bike is already there"
   end
+
+  it "Raises an error" do
+    expect {subject.release_bike}.to raise_error("There are no bikes here")
+  end
+
+
 
 end
